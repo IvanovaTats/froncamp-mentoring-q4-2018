6c71252f55cd835e0ec7c819cf82f5a5db10d76f
Tatsiana Ivanova <Tatsiana_Ivanova@epam.com>
2019-02-10 21:02:56 +0300

    feat: add articles-list

--- angularApp/src/app/app.component.ts
+++ angularApp/src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <app-articles></app-articles>
+    </div>
+    `
 })
 export class AppComponent {
-  title = 'angularApp';
+  pageTitle = 'Source Name';
 }
--- angularApp/src/app/app.module.ts
+++ angularApp/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleListComponent } from 'src/articles-list/articles-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleListComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
